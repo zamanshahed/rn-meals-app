e0e64f3fed16e3bf2e86ecb81b59fb9478c8299c
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-04-28 23:29:23 +0600

    imports fixed,  appLoading and Font bug fixed

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-// import * as Font from "expo-font";
-// import { AppLoading } from "expo-app-loading";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
 
 import MealsNavigator from "./Navigation/MealsNavigator";
 
-// const fontFetch = () => {
-//   return Font.loadAsync({
-//     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-//     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-//   });
-// };
+const fontFetch = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
-  // const [fontLoaded, setFontLoaded] = useState(false);
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-  // if (!fontLoaded) {
-  //   return (
-  //     <AppLoading
-  //       startAsync={fontFetch}
-  //       onFinish={() => setFontLoaded(true)}
-  //       onError={(err) => console.log(err)}
-  //     />
-  //   );
-  // }
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fontFetch}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
 
   return <MealsNavigator />;
 }
