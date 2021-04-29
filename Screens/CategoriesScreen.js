7486a4ec7bdbfe3d30845a68022f4618c52fbaed
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-04-29 12:48:16 +0600

    reactNavigation functional now

--- Screens/CategoriesScreen.js
+++ Screens/CategoriesScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 const CategoriesScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>Categories Screen!</Text>
+      <Button
+        title="Meals Screen"
+        onPress={() => {
+          props.navigation.navigate("CategoryMeals");
+        }}
+      />
     </View>
   );
 };
--- Screens/CategoryMealsScreen.js
+++ Screens/CategoryMealsScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 const CategoryMealsScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>Category Meals Screen !</Text>
+      <Button
+        title="Meal Details"
+        onPress={() => {
+          props.navigation.navigate("MealDetails");
+        }}
+      />
     </View>
   );
 };
