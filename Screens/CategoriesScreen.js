1cb47258b7639339e5233dd99182ed01526beeb3
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-05-01 15:39:55 +0600

    globally colors added, platform based header styling

--- Screens/CategoriesScreen.js
+++ Screens/CategoriesScreen.js
@@ -6,9 +6,11 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
+  Platform,
 } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
@@ -29,6 +31,14 @@ const CategoriesScreen = (props) => {
   return (
     <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   );
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
