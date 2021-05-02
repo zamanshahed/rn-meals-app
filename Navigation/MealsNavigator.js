730b239762bee88e9a5106991d3c87678383f016
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-05-02 23:23:56 +0600

    moving header styling to mealsNavigation in order to stop repeatation, more to go

--- Navigation/MealsNavigator.js
+++ Navigation/MealsNavigator.js
@@ -1,14 +1,23 @@
+import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
 import CategoriesScreen from "../Screens/CategoriesScreen";
 import CategoryMealsScreen from "../Screens/CategoryMealsScreen";
 import MealDetailsScreen from "../Screens/MealDetailsScreen";
+import Colors from "../constants/Colors";
 
 const MealsNavigator = createStackNavigator({
   Categories: CategoriesScreen,
   CategoryMeals: {
     screen: CategoryMealsScreen,
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+      },
+      headerTintColor:
+        Platform.OS === "android" ? "white" : Colors.primaryColor,
+    },
   },
   MealDetails: MealDetailsScreen,
 });
