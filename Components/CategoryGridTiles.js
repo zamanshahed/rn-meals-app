447807d39056c3580f1f839c7b88c585e8b3aec9
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-05-04 01:33:42 +0600

    more styling added to categories screen, touchableOpacity will be kept on android too this time

--- Components/CategoryGridTiles.js
+++ Components/CategoryGridTiles.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { version } from "react";
 import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
 
 const CategoryGridTiles = (props) => {
   return (
     <TouchableOpacity style={styles.gridItem} onPress={props.onSelect}>
-      <View style={{ backgroundColor: props.color }}>
-        <Text>{props.title}</Text>
+      <View
+        style={{
+          ...styles.categoryContainer,
+          ...{ backgroundColor: props.color },
+        }}
+      >
+        <Text style={styles.categoryTitle} numberOfLines={2}>
+          {props.title}
+        </Text>
       </View>
     </TouchableOpacity>
   );
@@ -15,7 +23,23 @@ const styles = StyleSheet.create({
   gridItem: {
     flex: 1,
     margin: 12,
-    height: 52,
+    height: 152,
+  },
+  categoryContainer: {
+    flex: 1,
+    borderRadius: 12,
+    shadowColor: "black",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 3 },
+    shadowRadius: 11,
+    elevation: 6,
+    padding: 13,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  categoryTitle: {
+    fontSize: 22,
+    textAlign: "right",
   },
 });
 
