bd72d00bb863d37a316de273eecabe2367b9af80
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-05-04 02:20:44 +0600

    categoryMealScreen is now shows the names of specific categories. styling needed

--- Screens/CategoryMealsScreen.js
+++ Screens/CategoryMealsScreen.js
@@ -1,28 +1,24 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
-import { CATEGORIES } from "../data//dummy-data";
+import { CATEGORIES, MEALS } from "../data//dummy-data";
 
 const CategoryMealsScreen = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+  };
   const catId = props.navigation.getParam("categoryId");
-  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
-  headerName = selectedCategory.title;
+  // const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+  const displayMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
   return (
     <View style={styles.screen}>
-      <Text>Category Meals Screen !</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title="Meal Details"
-        onPress={() => {
-          props.navigation.navigate("MealDetails");
-        }}
-      />
-      <Button
-        title="Back"
-        onPress={() => {
-          props.navigation.pop();
-        }}
-      />
+      <FlatList data={displayMeals} renderItem={renderMealItem} />
     </View>
   );
 };
